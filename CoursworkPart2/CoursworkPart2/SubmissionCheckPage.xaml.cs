139325294849eb4f0f142d5a254166f7d39079df
Philip Bell <philhabell@gmail.com>
2016-11-23 12:06:25 +0000

    Added the pages for people to work on

--- CoursworkPart2/CoursworkPart2/SubmissionCheckPage.xaml.cs
+++ CoursworkPart2/CoursworkPart2/SubmissionCheckPage.xaml.cs
@@ -13,23 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace CoursworkPart2
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class SubmissionCheckPage : Page
     {
-        public MainPage()
+        public SubmissionCheckPage()
         {
             this.InitializeComponent();
-        }
-
-        private void textBlock_SelectionChanged(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
